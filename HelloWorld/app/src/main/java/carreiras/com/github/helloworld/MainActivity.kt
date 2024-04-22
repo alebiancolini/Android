3495d72c3e0909395324a552471f87baf0458063
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-04-22 09:36:55 -0300

    3.HelloWorld - Desenhando view utilizando arquivos de layout

--- HelloWorld/app/src/main/java/carreiras/com/github/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/carreiras/com/github/helloworld/MainActivity.kt
@@ -8,11 +8,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Criando um objeto de texto
-        val texto = TextView(this)
-        texto.text = "Hello	Kotlin"
-
         // Definindo o conteúdo da tela
-        setContentView(texto)
+        setContentView(R.layout.activity_main)
     }
 }
